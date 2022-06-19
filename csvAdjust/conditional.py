a4647d972face2de5791f2c530a0a01d85d5e897
Shaun <shaun.jss@gmail.com>
2022-06-19 14:00:46 -0500

    Added parsing for the conditionals

--- csvAdjust/conditional.py
+++ csvAdjust/conditional.py
@@ -2,6 +2,14 @@ import logging
 from enum import Enum
 from abc import ABC, abstractmethod
 
+class FieldType(Enum):
+    """Enumeration of the valid kinds of required fields a conditional can have"""
+    COLUMN_NUMBER = ("columnnumber")
+    VALUE = ("value")
+    
+    def __init__(self, jsonName):
+        self.jsonName = jsonName
+        
 class Conditional(ABC):
     """Abstract class that concrete conditional classes extend. This might not be that necessary, but my day job
     is Java development, and I feel the need to make an interface even in Python."""
@@ -12,6 +20,45 @@ class Conditional(ABC):
         NOTE: The row object is 0-indexed
         """
         pass
+    
+    @classmethod
+    @abstractmethod
+    def get_required_fields(cls):
+        """This method will return the fields required by the conditional"""
+        
+    @staticmethod
+    def parse_conditionals(conditional_list):
+        """Takes a list of dictionaries that contain conditional configurations and transforms them into conditional objects"""
+        conditionals = []
+        for condition in conditional_list:
+            conType = ConditionalTypes.get_type(condition['type'])
+            
+            # If we can't figure out the type, throw an exception to prevent unintended wonky stuff from happening in the output files
+            if conType is None:
+                logging.error("Unable to determine conditional type '%s'. Available conditional types are '%s'", 
+                              condition['type'], ', '.join(str(e.jsonName) for e in conTypes))
+                
+                raise Exception("Unable to determine conditional type")
+            
+            # Get the required fields of this condition
+            requiredFields = conType.implementation.get_required_fields()
+            
+            # Now lets get the fields from the configuration
+            fieldValues = []
+            for field in requiredFields:
+                # Throw an exception if a required field is missing
+                if field.jsonName not in condition:
+                    logging.error("Unable to find required field '%s' for a '%s' condition",
+                                  field.jsonName, conType.name)
+                    raise Exception("Missing required field for conditional")
+                
+                fieldValues = condition[field.jsonName]
+                
+            # Now lets actually create the conditional objects. 
+            # We can pass in the values stored in the fieldValues list as arguments to the constructor by unpacking the list
+            # We do that by putting a * in front of the variable name
+            conditionals.append(conType.implementation(*fieldValues))  
+                
     
 class ColumnEquals(Conditional):
     """Checks for column equality to a predetermined value"""
@@ -39,6 +86,10 @@ class ColumnEquals(Conditional):
         """
         return row[self.columnNumber] == self.value
     
+    @classmethod
+    def get_required_fields(cls):
+        return (FieldType.COLUMN_NUMBER, FieldType.VALUE)
+    
 class ColumnContains(Conditional):
     """Checks if a column contains a value"""
     
@@ -47,32 +98,61 @@ class ColumnContains(Conditional):
         self.value = value
         
     def is_met(self, row):
-        """Checks if the value is in the column"""
+        """"Checks if column contains the value
+        
+        Parameters
+        ----------
+        row : list
+            The values of an entire row in a CSV. Each element is a different column
+        """
         return self.value in row[self.columnNumber]
+    
+    @classmethod
+    def get_required_fields(cls):
+        return (FieldType.COLUMN_NUMBER, FieldType.VALUE)
     
 class RowContains(Conditional):
     """Checks if a value exists in any of the columns"""
     
-    def __init__(self, columnNumber, value):
+    def __init__(self, value):
         """We don't actually need columnNumber, but it is included to follow the patter"""
-        self.columnNumber = columnNumber
         self.value = value
          
     def is_met(self, row):
+        """"Checks if the value exists anywhere in this row
+        
+        Parameters
+        ----------
+        row : list
+            The values of an entire row in a CSV. Each element is a different column
+        """
         for column in row:
             if self.value in column:
                 return True
             
         return False
     
+    @classmethod
+    def get_required_fields(cls):
+        return (FieldType.COLUMN_NUMBER)
+    
 class ConditionalTypes(Enum):
     """The types of conditionals that can be used"""
     
-    COLUMN_EQUALS = ("Column Equals", "columnequals", ColumnEquals)
-    COLUMN_CONTAINS = ("Column Contains", "columncontains", ColumnContains)
-    ROW_CONTAINS = ("Row Contains", "rowcontains", RowContains)
+    COLUMN_EQUALS = ("columnequals", ColumnEquals)
+    COLUMN_CONTAINS = ("columncontains", ColumnContains)
+    ROW_CONTAINS = ("rowcontains", RowContains)
+    
 
-    def __init__(self, id, jsonName, implementation):
-        self.id = id
+    def __init__(self, jsonName, implementation):
         self.jsonName = jsonName
         self.implementation = implementation
+        
+    @classmethod
+    def get_type(cls, name):
+        """Returns the ConditionalType that matches the string passed in"""
+        for conditional in cls:
+            if conditional.jsonName is name:
+                return conditional
+            
+        return None
--- test/conditionalTest.py
+++ test/conditionalTest.py
@@ -33,13 +33,63 @@ class ConditionalTest(unittest.TestCase):
         """Tests the row contains conditional"""
         row = ConditionalTest.get_default_list()
         
-        contains = conditional.RowContains(None, "urp")
+        contains = conditional.RowContains("urp")
         
         self.assertTrue(contains.is_met(row))
         
         row[5] = "White"
         
         self.assertFalse(contains.is_met(row))
+        
+    def test_conditional_types_enum(self):
+        """Tests for the ConditionalTypes enumeration"""
+        conTypes = conditional.ConditionalTypes
+        
+        self.assertEqual(3, len(conTypes))
+        
+        self.assertEqual(conTypes.get_type("columnequals"), conTypes.COLUMN_EQUALS)
+        self.assertEqual(conTypes.get_type("columncontains"), conTypes.COLUMN_CONTAINS)
+        self.assertEqual(conTypes.get_type("rowcontains"), conTypes.ROW_CONTAINS)
+        
+    def test_conditional_parsing(self):
+        testConfig = [
+            {
+                "type":"columnequals",
+                "columnnumber":2,
+                "value":"whatever"
+            },
+            {
+                "type":"columncontains",
+                "columnnumber":7,
+                "value":9.2
+            },
+            {
+                "type":"rowcontains",
+                "value":"Mango"
+            }
+        ]
+        
+        conditionals = conditional.Conditional.parse_conditionals(testConfig)
+        
+        # Verify the correct amount were created
+        self.assertEqual(len(conditionals), 3)
+        
+        # Verify the correct objects where created
+        conTypes = conditional.ConditionalTypes
+        
+        self.assertEqual(type(conditionals[0]), conTypes.COLUMN_EQUALS.implementation)
+        self.assertEqual(type(conditionals[1]), conTypes.COLUMN_CONTAINS.implementation)
+        self.assertEqual(type(conditionals[2]), conTypes.ROW_CONTAINS.implementation)
+        
+        #Missing required field value
+        brokeConfig = [
+            {
+                "type":"columnequals",
+                "columnnumber":2
+            }
+        ]
+        self.assertRaises(Exception, callable)
+        
 
     @staticmethod
     def get_default_list():
